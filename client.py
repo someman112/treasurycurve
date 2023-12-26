b4b07559e7455ca1ae71d69df7a88ee7e2efa95c
abdullah <abdullahsiddiqui444@gmail.com>
2023-12-25 22:41:10 -0500

    Final

--- client.py
+++ client.py
@@ -1,13 +1,29 @@
-import asyncio
-import websockets
+import timeit
 
-async def receive_data():
-    async with websockets.connect('ws://localhost:8765') as websocket:
-        symbol = input("Enter a symbol: ")
-        await websocket.send(symbol)
-        while True:
-            data = await websocket.recv()
-            print("Received data:", data)
 
-asyncio.get_event_loop().run_until_complete(receive_data())
+# import asyncio
+# import websockets
+#
+# async def receive_data():
+#     async with websockets.connect('ws://localhost:8765') as websocket:
+#         symbol = input("Enter a symbol: ")
+#         await websocket.send(symbol)
+#         while True:
+#             data = await websocket.recv()
+#             print("Received data:", data)
+#
+# asyncio.get_event_loop().run_until_complete(receive_data())
+#
 
+def reverse_string_recur(s: str):
+    if s == '':
+        return s
+    else:
+        return s[-1] + reverse_string_recur(s[:len(s) - 1])
+
+
+def reverse_string_iter(s: str):
+    new_str = ''
+    for i in range(len(s) - 1, -1, -1):
+        new_str += s[i]
+    return new_str
--- xauusd.py
+++ xauusd.py
@@ -5,8 +5,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import websockets
+import json
 
 connected_clients = set()
+waiting_clients = set()
 
 options = Options()
 options.add_argument('--headless')
@@ -21,26 +23,48 @@ async def handle_client(websocket):
     global previous_price
     connected_clients.add(websocket)
     try:
-        symbol = await websocket.recv()
-        link = base_link + symbol
-        driver.get(link)
-
         while True:
-            dataa = generate_data()
+            symbol = await websocket.recv()
+            link = base_link + symbol
+            driver.get(link)
+            while True:
+                mkt_open = driver.find_element(By.CLASS_NAME, "text-VzJVlozY")
+                mkt_open_text = mkt_open.get_attribute("textContent")
+                if mkt_open_text == "Market open" or mkt_open_text == "Post-market":
+                    title = driver.find_element(By.CLASS_NAME, "title-HFnhSVZy")
+                    sym_change = driver.find_element(By.CLASS_NAME, "js-symbol-change")
+                    sym_change_pt = driver.find_element(By.CLASS_NAME, "js-symbol-change-pt")
+                    pic = driver.find_element(By.CLASS_NAME, "large-xoKMfU7r")
+                    dataa = None
+                    if mkt_open_text == "Market open":
+                        dataa = generate_data()
+                    elif mkt_open_text == "Post-market":
+                        dataa = generate_data_post_mkt()
 
-            if dataa != previous_price:
-                await websocket.send(str(dataa))
-                previous_price = dataa
+                    if dataa != previous_price:
+                        curr = [title.text, str(dataa), sym_change.text, sym_change_pt.text, pic.get_attribute("src")]
+                        if mkt_open_text == "Post-market":
+                            curr.append(mkt_open_text)
 
-            try:
-                symbol = await asyncio.wait_for(websocket.recv(), timeout=1)
-                link = base_link + symbol
-                driver.get(link)
-            except asyncio.TimeoutError:
-                pass
+                        to_send = json.dumps(curr)
+                        await websocket.send(to_send)
+                        previous_price = dataa
 
+                    try:
+                        symbol = await asyncio.wait_for(websocket.recv(), timeout=1)
+                        link = base_link + symbol
+                        driver.get(link)
+                    except asyncio.TimeoutError:
+                        pass
+
+                else:
+                    curr = [str(mkt_open_text)]
+                    to_send = json.dumps(curr)
+                    await websocket.send(to_send)
+                    break
     except websockets.exceptions.ConnectionClosed:
         pass
+
     finally:
         connected_clients.remove(websocket)
 
@@ -57,6 +81,17 @@ async def start_server():
 
 def generate_data():
     price_element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "quotesRow-pAUXADuj")))
+    price = ''
+    for i in price_element.text.split("\n")[0]:
+        if i.isdigit() or i == ".":
+            price += i
+
+    if price != '':
+        return float(price)
+
+
+def generate_data_post_mkt():
+    price_element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "js-symbol-ext-hrs-close")))
     price = ''
     for i in price_element.text.split("\n")[0]:
         if i.isdigit() or i == ".":
